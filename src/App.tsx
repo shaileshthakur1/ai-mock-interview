cbdb158b9e2656bc5755d1d7c6d01a1a22dac6f2
Shailesh Thakur <thakurshaileshkumar507@gmail.com>
2025-03-21 14:49:55 +0530

    Project initiated with Shadcn

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <>
+      <div className="text-blue-500">App</div>
+      <Button variant={"outline"}>CLICK ME</Button>
+    </>
+  );
+;}
+
+export default App
